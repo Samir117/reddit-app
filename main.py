24d1d63d22185ece533ec89758074079f708b5d0
Samir Rojas <samirrojas42@gmail.com>
2023-10-15 20:36:20 -0500

    Final

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 import csv
 from googletrans import Translator
+from datetime import datetime
 
 # Configura las credenciales OAuth2
 client_id = "cJch1PistFynpir_8PhcpQ"
@@ -23,7 +24,7 @@ search_query = "facebook"
 subreddit = reddit.subreddit(subreddit_name)
 
 # Número total de resultados que deseas obtener
-total_results = 50  # Puedes cambiar este valor según tus necesidades
+total_results = 150  # Puedes cambiar este valor según tus necesidades
 
 # Lista para almacenar todos los resultados
 all_results = []
@@ -35,25 +36,32 @@ for submission in subreddit.search(search_query, limit=total_results):
 # Nombre del archivo CSV
 csv_file = "resultados.csv"
 
-# Abre el archivo CSV en modo escritura
+# Listas para calcular la media de Puntuación y Cantidad de Comentarios
+scores = []
+comment_counts = []
+
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
 
-    # Define el orden de los campos en el encabezado del CSV
-    header = ["ID", "Autor", "URL", "Puntuación", "Cantidad de Comentarios", "Creado", "Texto Original", "Texto Traducido"]
+    # Escribe el encabezado
+    writer.writerow(["ID", "Autor", "URL", "Puntuación", "Cantidad de Comentarios", "Creado", "Texto Original", "Texto Traducido"])
 
-    # Escribe la fila de encabezados en el archivo CSV
-    writer.writerow(header)
-
-    # Configura el traductor
     translator = Translator()
 
-    # Itera a través de todos los resultados y escribe cada conjunto de datos en una fila del archivo CSV
     for submission in all_results:
-        # Traduce el título al español
+        scores.append(submission.score)
+        comment_counts.append(submission.num_comments)
+
         titulo_es = translator.translate(submission.title, src='auto', dest='es').text
-        # Escribe los datos en el CSV en el orden especificado
-        row = [submission.id, submission.author, submission.url, submission.score, submission.num_comments, submission.created_utc, submission.title, titulo_es]
-        writer.writerow(row)
+
+        # Reemplaza '0' por la media de Puntuación y Cantidad de Comentarios
+        avg_score = sum(scores) / len(scores)
+        avg_comment_count = sum(comment_counts) / len(comment_counts)
+
+        score = avg_score if submission.score == 0 else submission.score
+        comment_count = avg_comment_count if submission.num_comments == 0 else submission.num_comments
+
+        # Escribe los datos en el archivo CSV
+        writer.writerow([submission.id, submission.author, submission.url, score, comment_count, submission.created_utc, submission.title, titulo_es])
 
 print(f'Se han exportado exitosamente {len(all_results)} resultados a {csv_file}.')
